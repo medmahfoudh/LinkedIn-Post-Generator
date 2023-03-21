aceb4429afaf07b2156f42d95c7b40851eab3fcb
medmahfoudh <medmahfoudhbouna@gmail.com>
2023-03-21 18:26:32 +0000

    21-03-023 V1

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import openai
 
 app = Flask(__name__)
 
-openai.api_key = "YOUR_API_KEY" 
+openai.api_key = "YOU_API_KEY" 
 
 @app.route('/' , methods = ["GET" , "POST"])
 def index():
     if request.method == 'POST':
         title = request.form['title']
         word = request.form['word']
-        post = generate_post(title, word)
+        post = generate_post(title , word)
         return render_template("post.html", post=post, title=title , word = word)
     return render_template("index.html")
 
@@ -22,14 +22,14 @@ def post():
 def generate_post(title , word):
     response = openai.Completion.create(
         engine="text-davinci-002",
-        prompt = f"I want to write a LinkedIn post that talks about me {title} , write the post in {word} words.",
+        prompt = f"I want to write a LinkedIn post that talks about me {title} , write the post in minimum {word} words.",
         # prompt=f"Write a LinkedIn post about {title} with minimum 40 words and hashtags at the end",
-        max_tokens=1000,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.7,
     )
-    return response.choices[0].text.strip()
+    return response.choices[0].text.strip() 
 
 
 
